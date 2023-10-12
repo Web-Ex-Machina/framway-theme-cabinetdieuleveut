2d9f1f271a54b33e3e259996bf94ba598ff063c1
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-10-12 18:00:26 +0200

    basic config

--- config.js
+++ config.js
@@ -1,1 +1,30 @@
-module.exports = {};
+module.exports = {
+	'colors' : {
+		'burgandy' : '#752D37',
+		'greystrong': '#59595B'
+	},
+	'primary': 'colors(burgandy)',
+	'secondary': 'colors(greystrong)',
+
+	'body': {
+		'background': 'colors(white)',
+		'block-background': 'colors(white)',
+		'font-color': 'colors(greystronger)',
+	},
+	'footer': {
+		'background': 'secondary',
+		'font-color': 'contrastFW(colors(white),secondary)',
+	},
+	'input': {
+		'background': 'colors(white)',
+		'font-color': 'colors(greystronger)',
+		'border-color': 'colors(greylighter)',
+		'placeholder-font-color': 'colors(grey)',
+		'radius': true,
+	},
+	'input-focus': {
+		'background': 'input(background)',
+		'font-color': 'input(font-color)',
+		'border-color': 'colors(grey)',
+	},
+};
